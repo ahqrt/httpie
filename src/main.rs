8a1756fb2081598d9526d525fc0a51a31e766a62
yangliye <1217223113@qq.com>
2022-09-16 21:48:33 +0800

    feat: 添加基础功能

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+use anyhow::{anyhow, Ok};
+use clap::Parser;
+use colored::*;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use std::{collections::HashMap, str::FromStr};
+
+#[derive(Debug, Parser)]
+#[clap(version = "1.0", author = "ahqrt")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Debug, Parser)]
+struct Get {
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+}
+
+#[derive(Debug, Parser)]
+struct Post {
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
+}
+
+#[derive(Debug, PartialEq)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair, anyhow::Error> {
+    Ok(s.parse()?)
+}
+
+fn parse_url(s: &str) -> Result<String, anyhow::Error> {
+    // 这里我们仅仅检查一下 URL 是否合法
+    let s = Url::parse(s)?;
+    Ok(s.into())
+}
+
+async fn get(client: Client, args: &Get) -> Result<(), anyhow::Error> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<(), anyhow::Error> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    print!("{}\n", status)
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}:{:?}", name.to_string().green(), value)
+    }
+    println!("\n");
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        _ => println!("{}", body),
+    }
+}
+
+async fn print_resp(resp: Response) -> Result<(), anyhow::Error> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_context_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+fn get_context_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+
+    let client = Client::builder().default_headers(headers).build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
+}
